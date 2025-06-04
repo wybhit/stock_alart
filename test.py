44c104efc223af3d3d781aa4d750e15889d77296
wybhit <wybhit@gmail.com>
2025-06-04 19:40:38 +0800

    测试一下

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-    
-from data.stock_data import MarketTimeTools
+import akshare as ak
 
-market_time_tools = MarketTimeTools()
-print(market_time_tools.is_market_time())           
+stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20170301", end_date='20231022', adjust="")
+print(stock_zh_a_hist_df)
